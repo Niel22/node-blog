be623374e2b594fb039aeec40685741dfef4c0f3
Niel22 <niel2264@gmail.com>
2024-12-27 10:24:05 +0100

    Good Morning

--- app/controller/AppController.js
+++ app/controller/AppController.js
@@ -5,9 +5,13 @@ class AppController{
 
     async index(req, res){
 
+        const locals = {
+            title: "Home",
+            description: "This is Home page"
+        }
         const posts = await FetchAllPost.execute();
 
-        return res.render('index', {posts: posts});
+        return res.render('index', {posts: posts, locals: locals});
     }
 
     async about(req, res){
